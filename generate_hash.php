2d28ea781ca659edb9a4d32d0e53438e90530db8
Aathi18 <athistakumar1804@gmail.com>
2025-06-23 10:15:37 +0530

    Update generate_hash.php

--- generate_hash.php
+++ generate_hash.php
@@ -1,5 +1,5 @@
 <?php
-$admin_password = 'admin123'; // <--- REPLACE THIS with your desired plaintext password
+$admin_password = 'shafee123'; // <--- REPLACE THIS with your desired plaintext password
 $hashed_password = password_hash($admin_password, PASSWORD_DEFAULT);
 echo "Your hashed password: " . $hashed_password;
 ?>
